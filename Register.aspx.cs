c2dac5bf170855b21286b214bf5c3a4a78d613dc
Bartosz Ociepka <bartosz.ociepka96@gmail.com>
2020-10-22 13:41:45 +0200

    Adding and modifying halls

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -14,6 +14,11 @@ public partial class Register : System.Web.UI.Page
 	protected void CreateAccountButton_Click(object sender, EventArgs e)
 	{
 		MembershipCreateStatus createStatus;
+		if (!IsValidEmail(Email.Text))
+		{
+			StatusMessage.Text = "There email address you provided in invalid.";
+			return;
+		}
 		MembershipUser newUser = Membership.CreateUser(Username.Text, Password.Text, Email.Text, passwordQuestion, SecurityAnswer.Text, true, out createStatus);
 		switch (createStatus)
 		{
@@ -42,6 +47,19 @@ public partial class Register : System.Web.UI.Page
 			default:
 				StatusMessage.Text = "There was an unknown error; the user account was NOT created.";
 				break;
+		}
+	}
+
+	bool IsValidEmail(string email)
+	{
+		try
+		{
+			var addr = new System.Net.Mail.MailAddress(email);
+			return addr.Address == email;
+		}
+		catch
+		{
+			return false;
 		}
 	}
 }
